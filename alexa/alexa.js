9e929092c01dd4f6ac94768470a45746459b0aa1
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-11 01:38:42 -0500

    intents solidified

--- alexa/alexa.js
+++ alexa/alexa.js
@@ -1,16 +1,23 @@
 'use strict';
 
 var Alexa = require("alexa-sdk");
+var message;
+var response;
 
 var handlers = {
     "talkingIntent" : function() {
-        this.response.speak("Nothing");
+        this.response.speak(message);
         this.emit(":responseReady");
     }
     "LaunchRequest": function () {
       this.response.speak("First Question");
       this.emit(":responseReady");
      }
+
+    "Answer":function(){
+      this.response.speak(response);
+      this.emit(":responseReady");
+    }
 
 }
 
--- answerQuestion.js
+++ answerQuestion.js
@@ -3,6 +3,5 @@ module['exports'] = function echoHttp (hook) {
     console.log(hook.req.path);
     console.log(hook.req.method);
     console.log(hook.env);
-
-  	var message = hook.params["text"]; 
+  	var message = hook.params["text"];
 };
