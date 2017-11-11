a7c993cddd2741d5331991c1e6798ba8d3a593fd
Yingning Gui <yingning.gui@uwaterloo.ca>
2017-11-11 07:56:45 -0500

    node

--- alexa/alexa.js
+++ alexa/alexa.js
@@ -1,13 +1,32 @@
 'use strict';
 
 var Alexa = require("alexa-sdk");
-var message;
-var response;
+var message = get.message();
+var response = get.response();
+
+function post() {
+  var request = require('request');
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+  });
+}
+
+function get() {
+  var request = require('request');
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+  });
+}
 
 var handlers = {
     "talkingIntent" : function() {
         this.response.speak(message);
         this.emit(":responseReady");
+        this.post();
     }
     "LaunchRequest": function () {
       this.response.speak("First Question");
@@ -17,9 +36,12 @@ var handlers = {
     "Answer":function(){
       this.response.speak(response);
       this.emit(":responseReady");
+      this.get();
     }
 
 }
+
+
 
 exports.handler = function(event, context, callback) {
 	var alexa = Alexa.handler(event,context);
