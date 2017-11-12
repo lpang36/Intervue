fbedd68a48e2c56e2ece1ca822bf95f9de3535bb
Lawrence Pang <lawrencepang36@gmail.com>
2017-11-12 09:57:42 -0500

    formatting

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function analyzeKeywords(params,question,tone,req,res) {
     });
     //var advice = "Your score on this question was "+score+". "+question.defaultAdvice;
     //var advice = question.defaultAdvice;
-    var advice = "Here are some tips on how to improve your answer";
+    var advice = "Here are some tips on how to improve your answer. ";
     
     var fillerText = params.textToAnalyze.toUpperCase();
     if((/\bUM\b/).test(fillerText) ||
@@ -154,19 +154,19 @@ function analyzeKeywords(params,question,tone,req,res) {
        (/\bAH\b/).test(fillerText) ||
        (/\bLIKE\b/).test(fillerText) ||
        (/\bOKAY\b/).test(fillerText))
-    advice+="Try to use less filler words ";
+    advice+="Try to use less filler words. ";
     else if ((tone[0] > 0.5) || (tone[4] > 0.5) || (tone[2] > 0.5)&&tone[7]>0.5)
-      advice+="Try to sound more positive and confident in your answer ";
+      advice+="Try to sound more positive and confident in your answer. ";
     else if((tone[0] > 0.5) || (tone[4] > 0.5) || (tone[2] > 0.5))
-      advice+="Try to sound more positive ";
+      advice+="Try to sound more positive. ";
     else if(tone[7] > 0.5)
-      advice+="Try to sound more confident in your answer ";
+      advice+="Try to sound more confident in your answer. ";
     else if (tone[3]>0.6&&tone[6]>0.6)
-      advice+="You did well by sounding positive and confident ";
+      advice+="You did well by sounding positive and confident. ";
     else if(tone[3] > 0.6)
-      advice+="You did well by sounding positive ";
+      advice+="You did well by sounding positive. ";
     else if(tone[6] > 0.6)
-      advice+="You did well by sounding confident ";
+      advice+="You did well by sounding confident. ";
     /*if (keywordMatches<0.5) {
       advice+="You should include some of these words or concepts in your reponse: ";
       question.keywords.forEach(function(word){
@@ -175,9 +175,9 @@ function analyzeKeywords(params,question,tone,req,res) {
     }*/
     
     if (length>700)
-      advice+="also Try to keep your response to a shorter time ";
+      advice+="Also try to keep your response to a shorter time. ";
     else if (length<50)
-      advice+="also Try to give a more detailed response ";
+      advice+="Also try to give a more detailed response. ";
     io.emit('answer',{advice: advice, username: req.params.username, tone: tone, score: score, answer: params.textToAnalyze});
     console.log(advice);
     res.send({
